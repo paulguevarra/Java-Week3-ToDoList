e91875e8da44369bc1aae6b856141a7341d437a0
Paul Guevarra <p.a.guevarra@gail.com>
2017-08-16 10:30:28 -0700

    add dao

---
+++ src/main/java/dao/TaskDao.java
@@ -0,0 +1,26 @@
+
+package dao;
+
+import models.Task;
+
+import java.util.List;
+
+/**
+ * Created by epicodus_staff on 7/18/17.
+ */
+public interface TaskDao {
+
+    //create
+    void add (Task task);
+    //read
+    List<Task> getAll();
+
+    Task findById(int id);
+    //update
+      void update(int id, String content);
+    //delete
+   void deleteById(int id);
+
+   void clearAllTasks();
+
+}
--- src/main/java/models/Task.java
+++ src/main/java/models/Task.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 public class Task {
 
     private String description;
-    private static ArrayList<Task> instances = new ArrayList<>();
     private boolean completed;
     private LocalDateTime createdAt;
     private int id;
@@ -15,20 +14,39 @@ public class Task {
         this.description = description;
         this.completed = false;
         this.createdAt = LocalDateTime.now();
-        instances.add(this);
-        this.id = instances.size();
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task task = (Task) o;
+
+        if (completed != task.completed) return false;
+        if (id != task.id) return false;
+        return description.equals(task.description);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = description.hashCode();
+        result = 31 * result + (completed ? 1 : 0);
+        result = 31 * result + id;
+        return result;
     }
 
     public String getDescription() {
         return description;
-    }
-
-    public static ArrayList<Task> getAll(){
-        return instances;
-    }
-
-    public static void clearAllTasks(){
-        instances.clear();
     }
 
     public boolean getCompleted(){
@@ -43,15 +61,7 @@ public class Task {
         return id;
     }
 
-    public static Task findById(int id){
-        return instances.get(id-1); //why minus 1? See if you can figure it out.
-    }
 
-    public void update(String content) {
-        this.description = content;
-    }
 
-    public void deleteTask(){
-        instances.remove(id-1); //same reason
-    }
+
 }
